fbac738201b261c200f5b61d72c6762c9bef73c1
vthelamb <vglamb8@gmail.com>
2022-01-19 19:02:50 +0000

    session-3

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,5 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+console.log('Hello Everyone');
+console.log(document.querySelector('p').innerHTML);
+console.log(document.querySelectorAll('.about'));
--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,11 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+const a = 5;
+const b = 10;
+let c = a + b;
+
+console.log("Original output: " + c);
+
+a = 20;
+
+console.log("After 'a' has been updated: " + c)
