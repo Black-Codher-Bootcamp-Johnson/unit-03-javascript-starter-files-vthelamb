52b68cd020fc18df73fe2a1633ba4145cd5d23ce
vthelamb <vglamb8@gmail.com>
2022-01-31 19:35:55 +0000

    session-06

--- session-6/index.js
+++ session-6/index.js
@@ -8,22 +8,13 @@ while (useCount <= 10){
     console.log (randomNumber)
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // ****************************************************TASK 2****************************************************
+console.log("=========================================");
 
+for (let counter = 0; counter <= 10; counter = counter + 1 ) {
+  const randomNumber = Math.round(Math.random() * 10);
+  console.log(randomNumber)
+}
 
 
 
