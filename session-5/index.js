865d9d06a8e39383e422b8a4c1f771bd3124a92c
vthelamb <vglamb8@gmail.com>
2022-01-31 18:21:22 +0000

    session-05

--- session-5/index.js
+++ session-5/index.js
@@ -1,27 +1,42 @@
 function getGrade(total){
-    if (total > 90) {
-      return ("A+")
+  let grade;
+
+      switch (true) {
+      case (total > 90):
+      grade = 'A+';
+      break;
+
+      case (total > 80):
+      grade = 'A';
+      break;
+
+      case (total > 70):
+      grade = 'B';
+      break;
+
+      case (total > 60):
+      grade = 'C';
+      break;
+
+      case (total > 50):
+      grade = 'D';
+      break;
+
+      case (total > 40):
+      grade = 'E';
+      break;
+
+      case (total > 30):
+      grade = 'F';
+      break;
+
+      case (total < 30):
+      grade = 'FAIL';
+      break;
+
+      default:
+      grade = 'Not found';
     }
-    else if (total > 80 && total <= 90)
-      return("A");
-
-    else if (total > 70 && total <= 80)
-      return("B");
-
-    else if (total > 60 && total <= 70)
-      return("C");
-
-    else if (total > 50 && total <= 60)
-      return("D");
-
-    else if (total > 40 && total <=50)
-      return("E");
-
-    else if(total > 30 && total <=40)
-      return("F");
-
-    else if (total <30)
-      return("FAIL");
+    return grade
 }
-
-console.log("Your grade is: " + getGrade(45)+"!");
+console.log("Your grade is: " + getGrade(76)+"!");
